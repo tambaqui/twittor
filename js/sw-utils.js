fb7711be9ee1a0a4ef460f2e5d15f9d88efcd78c
Raúl FS <rfsolana@gmail.com>
2022-05-09 10:04:08 +0200

    twittor init

---
+++ js/sw-utils.js
@@ -0,0 +1,13 @@
+// Guardar en el caché dinamico
+function actualizaCacheDinamico( dynamicCache, req, res ){
+
+    if ( res.ok ) {
+        return caches.open( dynamicCache ).then ( cache => {
+            cache.put(req, res.clone() );
+            return res.clone();
+        });
+    } else {
+        return res;
+    }
+
+}
